7c230cf3f0a1b4fcdd27b81e221e0e3a7fd1881f
kisdy <bwply2009@163.com>
2021-05-04 23:14:24 +0800

    esProduct elasticsearch 實現商品搜索

--- src/main/java/com/macro/mall/com/api/CommonPage.java
+++ src/main/java/com/macro/mall/com/api/CommonPage.java
@@ -1,6 +1,7 @@
 package com.macro.mall.com.api;
 
 import com.github.pagehelper.PageInfo;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -29,6 +30,19 @@ public class CommonPage<T> {
         return result;
     }
 
+    /**
+     * 将SpringData分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(Page<T> pageInfo) {
+        CommonPage<T> result = new CommonPage<T>();
+        result.setTotalPage(pageInfo.getTotalPages());
+        result.setPageNum(pageInfo.getNumber());
+        result.setPageSize(pageInfo.getSize());
+        result.setTotal(pageInfo.getTotalElements());
+        result.setList(pageInfo.getContent());
+        return result;
+    }
+    
     public Integer getPageNum() {
         return pageNum;
     }
